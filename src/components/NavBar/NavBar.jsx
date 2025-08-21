4cb060cdd7c0fbe7b915cdb6d9a5bb33a1484ec4
mrodavis <orville.davis@scrapit.io>
2025-08-21 13:07:35 -0400

    added initial phase of styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react';
 import { Link } from 'react-router';
+import styles from './NavBar.module.css';
+import Logo from '../../assets/images/logo.svg';
 
 import { UserContext } from '../../contexts/UserContext';
 
@@ -12,7 +14,8 @@ const NavBar = () => {
   };
 
   return (
-    <nav>
+    <nav className={styles.container}>
+      <Link to='/'><img src={Logo} alt='A cute owl' /></Link>
       {user ? (
         <ul>
           <li><Link to='/'>HOME</Link></li>
